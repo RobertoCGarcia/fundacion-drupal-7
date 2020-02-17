429d83079493d33c1859123b07cedc67bb545d13
Roberto Garcia <roberto.garcia@my.unitec.edu.mx>
2020-02-17 09:56:27 -0600

    Cambios Zumpango Website

--- sites/all/themes/finances/page.tpl.php
+++ sites/all/themes/finances/page.tpl.php
@@ -146,22 +146,22 @@ $path = drupal_get_path_alias();
         <div class="col-md-9">
           <div class="row">
             <div class="col-md-5">
-              <h2 class="footer-heading mb-4">About Us</h2>
+              <h2 class="footer-heading mb-4">Acerca de Nosotros</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
             </div>
             <div class="col-md-3 ml-auto">
-              <h2 class="footer-heading mb-4">Quick Links</h2>
+              <h2 class="footer-heading mb-4">Links Rápidos</h2>
               <ul class="list-unstyled">
-                <li><a href="#about-section" class="smoothscroll">Terms</a></li>
-                <li><a href="#about-section" class="smoothscroll">Policy</a></li>
-                <li><a href="#about-section" class="smoothscroll">About Us</a></li>
-                <li><a href="#services-section" class="smoothscroll">Services</a></li>
-                <li><a href="#testimonials-section" class="smoothscroll">Testimonials</a></li>
-                <li><a href="#contact-section" class="smoothscroll">Contact Us</a></li>
+                <li><a href="#about-section" class="smoothscroll">Terminos</a></li>
+                <li><a href="#about-section" class="smoothscroll">Política de Privacidad</a></li>
+                <li><a href="#about-section" class="smoothscroll">Acerca de Nosotros</a></li>
+                <li><a href="#services-section" class="smoothscroll">Servicios</a></li>
+                <li><a href="#testimonials-section" class="smoothscroll">Testimonios</a></li>
+                <li><a href="#contact-section" class="smoothscroll">Contactanos</a></li>
               </ul>
             </div>
             <div class="col-md-3 footer-social">
-              <h2 class="footer-heading mb-4">Follow Us</h2>
+              <h2 class="footer-heading mb-4">Síguenos en Redes Sociales</h2>
               <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
               <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
               <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
@@ -170,7 +170,7 @@ $path = drupal_get_path_alias();
           </div>
         </div>
         <div class="col-md-3">
-          <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
+          <h2 class="footer-heading mb-4">Inscríbete a nuestro Newsletter</h2>
           <form action="#" method="post" class="footer-subscribe">
             <div class="input-group mb-3">
               <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
@@ -181,16 +181,13 @@ $path = drupal_get_path_alias();
           </form>
         </div>
       </div>
+      
       <div class="row pt-5 mt-5 text-center">
         <div class="col-md-12">
           <div class="border-top pt-5">
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             <p>Copyright &copy;
-              <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made
-              with <i class="icon-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+              <script>document.write(new Date().getFullYear());</script>  Fundación GEER | Todos los Derechos Reservados
             </p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
           </div>
         </div>
 
